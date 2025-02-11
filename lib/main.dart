5683ef46c413046f8fd210f44b26be9a4821590b
ashlesh <tashleshkini@gmail.com>
2025-02-12 03:45:34 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:agri_smart/pages/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'firebase_options.dart';
