531c3d041051dc9b4d4a076884f0d235b6c5a215
ashlesh <tashleshkini@gmail.com>
2025-02-12 13:04:12 +0530

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,34 @@ import 'package:agri_smart/pages/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
 import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform );
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter("hive_boxes");
   await Hive.openBox('hive_boxes');
+  await Future.delayed(Duration(seconds: 3));
+  FlutterNativeSplash.remove();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Agri Smart',
-      home: SignInScreen() ,
+      home: SignInScreen(),
     );
   }
 }
-
